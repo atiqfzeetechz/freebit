21b5699f5b892a829863f583ecfe26f9e89cfa71
developer110_Atiq <developer109.fzeetechz@gmail.com>
2025-06-25 19:32:54 +0530

    --url

--- src/hooks/useAxios.tsx
+++ src/hooks/useAxios.tsx
@@ -29,8 +29,8 @@ interface errorRes<T = any> {
 }
 
 
-// export const baseUrl =`https://backend.freebit.fzeetechz.com/api/v1`;
-export const baseUrl =`http://192.168.1.21:5013/api/v1`;
+export const baseUrl =`https://backend.freebit.fzeetechz.com/api/v1`;
+// export const baseUrl =`http://192.168.1.21:5013/api/v1`;
 export const imgUrl = `https://backend.freebit.fzeetechz.com`
 
 export default function useAxios() {
